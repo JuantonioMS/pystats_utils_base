011002a6142640f9fa606454d9cb5ecdcd445a8b
JuanAntonioMS <b32masaj@gmail.com>
2022-11-07 17:18:45 +0100

    ROC Bootstrapping

--- pystats_utils/test/multivariant/logistic_regression.py
+++ pystats_utils/test/multivariant/logistic_regression.py
@@ -11,6 +11,8 @@ from sklearn.metrics import f1_score
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import roc_curve
 
+from sklearn.utils import resample
+
 
 from pystats_utils.test.multivariant import Multivariant
 
@@ -57,6 +59,53 @@ class LogisticRegression(Multivariant):
         #  ROC curve info
         prediction = model.predict()
 
+        # Bootstrapping
+
+        aucROCs = []
+        cutOffs = []
+        accuracies = []
+        errors = []
+        precisions = []
+        recalls = []
+        f1s = []
+
+        for _ in range(2000):
+
+                bootExpectedClass, bootPrediciton = resample(workingData[classVariable],
+                                                             prediction)
+
+                aucROC = roc_auc_score(bootExpectedClass,
+                                       bootPrediciton)
+
+                fpr, tpr, thresholds = roc_curve(bootExpectedClass,
+                                                 bootPrediciton)
+
+                aux = np.arange(len(tpr))
+                roc = pd.DataFrame({'tf' : pd.Series(abs(tpr - (1-fpr)), index = aux),
+                                    'thresholds' : pd.Series(thresholds, index = aux)})
+
+                threshold = list(roc.iloc[(roc.tf).abs().argsort()[:1]]["thresholds"])[0]
+
+                #  Confusion matrix info
+
+                expectedClass = bootExpectedClass
+                predictedClass = [1 if pred > threshold else 0 for pred in prediction]
+
+                accuracy = accuracy_score(expectedClass, predictedClass)
+                error = 1 - accuracy
+                precision = precision_score(expectedClass, predictedClass)
+                recall = recall_score(expectedClass, predictedClass)
+                f1 = f1_score(expectedClass, predictedClass)
+
+                aucROCs.append(aucROC)
+                cutOffs.append(threshold)
+                accuracies.append(accuracy)
+                errors.append(error)
+                precisions.append(precision)
+                recalls.append(recall)
+                f1s.append(f1)
+
+
         aucROC = roc_auc_score(workingData[classVariable],
                                prediction)
 
@@ -80,14 +129,30 @@ class LogisticRegression(Multivariant):
         recall = recall_score(expectedClass, predictedClass)
         f1 = f1_score(expectedClass, predictedClass)
 
+        values = [aucROC, threshold, accuracy, error, precision, recall, f1]
+        boots = [aucROCs, thresholds, accuracies, errors, precisions, recalls, f1s]
 
-        summary = pd.DataFrame({"Area under curve ROC" : [aucROC],
-                                "Best Cutoff" : [threshold],
-                                "Accuracy" : [accuracy],
-                                "Error" : [error],
-                                "Precision" : [precision],
-                                "Recall" : [recall],
-                                "F1" : [f1]})
+
+
+
+        lowerCI = [original - 1.97 * np.std(boot)   for original, boot in zip(values, boots)]
+        lowerCI = [ci if ci > 0 else 0.0 for ci in lowerCI]
+
+
+        upperCI = [original + 1.97 * np.std(boot)   for original, boot in zip(values, boots)]
+        upperCI = [ci if ci < 1 else 1.0 for ci in upperCI]
+
+
+        summary = pd.DataFrame({"Value"    : values,
+                                "CI 2.5%"  : lowerCI,
+                                "CI 97.5%" : upperCI},
+                               index = ["Area under curve ROC",
+                                        "Best Cutoff",
+                                        "Accuracy",
+                                        "Error",
+                                        "Precision",
+                                        "Recall",
+                                        "F1"])
 
         prediction = pd.Series(prediction,
                                index = workingData.index)
